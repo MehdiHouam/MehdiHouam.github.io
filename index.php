37a3aff7799aa67344c4f4f3d474f8a726c9e29d
unknown <mhwkswrd@gmail.com>
2021-08-04 19:59:21 +0200

    test sans php

--- index.php
+++ index.php
@@ -16,5 +16,20 @@
 
 <br><br><br>
 <button>CLICK HERE</button>
+
+
+<?php
+echo "Hehe";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
 </body>
 </html>
