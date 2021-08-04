9d0cfb5bcc201028902d7ad246b39cab9fedbf65
unknown <mhwkswrd@gmail.com>
2021-08-04 19:56:25 +0200

    test sans php

--- index.php
+++ index.php
@@ -4,23 +4,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Mehdi Houam</title>
 </head>
 <body>
+    <h1>Welcome to my space</h1>
+<aside>this is a description of my cloud</aside>
 
-<?php
-echo "Hehe";
-$path = ".";
-$dh = opendir($path);
-$i=1;
-while (($file = readdir($dh)) !== false) {
-    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
-        echo "<a href='$path/$file'>$file</a><br /><br />";
-        $i++;
-    }
-}
-closedir($dh);
-?> 
+<p>please dont try to hakc or you you will be hakced</p>
 
+<button>DONT CLICK HERE</button>
+
+<br><br><br>
+<button>CLICK HERE</button>
 </body>
 </html>
